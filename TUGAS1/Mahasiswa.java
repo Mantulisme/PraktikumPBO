f9bea5fcb6ba4e27d34f40353ef5a67e46430a6d
Mantulisme <114640193+Mantulisme@users.noreply.github.com>
2023-10-11 22:30:51 +0700

    Add files via upload

---
+++ TUGAS1/Mahasiswa.java
@@ -0,0 +1,15 @@
+package PBO3.TUGAS1;
+
+public class Mahasiswa {
+  public void membaca() {
+    System.out.println("Mahasiswa sedang membaca");
+  }
+
+  public void nyontek() {
+    System.out.println("Mahasiswa sedang nyontek");
+  }
+
+  public void modifikasi() {
+    System.out.println("Mahasiswa sedang memodifikasi");
+  }
+}
---
+++ TUGAS1/MahasiswaBeraksi.java
@@ -0,0 +1,12 @@
+package PBO3.TUGAS1;
+
+public class MahasiswaBeraksi {
+  public static void main(String[] args) {
+
+    Mahasiswa mahasiswa = new Mahasiswa();
+
+    mahasiswa.membaca();
+    mahasiswa.nyontek();
+    mahasiswa.modifikasi();
+  }
+}
